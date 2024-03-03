bc4a9cda8726ce1c98a9b14bf519e0fce143959f
Nizami Sevindi <nsevindi87@gmail.com>
2024-03-03 22:19:43 +0100

    mapping get

--- src/main/java/com/javaCodePractice/crud_dashboard/controller/UserController.java
+++ src/main/java/com/javaCodePractice/crud_dashboard/controller/UserController.java
@@ -3,13 +3,13 @@ package com.javaCodePractice.crud_dashboard.controller;
 import com.javaCodePractice.crud_dashboard.entity.User;
 import com.javaCodePractice.crud_dashboard.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
@@ -18,5 +18,10 @@ public class UserController {
     @PostMapping("/user")
     public User postUser(@RequestBody User user){
         return  userService.postUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
     }
 }
--- src/main/java/com/javaCodePractice/crud_dashboard/service/UserService.java
+++ src/main/java/com/javaCodePractice/crud_dashboard/service/UserService.java
@@ -5,6 +5,8 @@ import com.javaCodePractice.crud_dashboard.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -13,5 +15,9 @@ public class UserService {
 
     public User postUser( User user){
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
