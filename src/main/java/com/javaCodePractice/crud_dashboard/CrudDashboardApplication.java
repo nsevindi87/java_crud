9ef75bfb1ea330fb5f2287b8a992f58bc209acc4
Nizami Sevindi <nsevindi87@gmail.com>
2024-03-03 12:13:45 +0100

    new project

---
+++ src/main/java/com/javaCodePractice/crud_dashboard/CrudDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.javaCodePractice.crud_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaCodePractice/crud_dashboard/CrudDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaCodePractice.crud_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
